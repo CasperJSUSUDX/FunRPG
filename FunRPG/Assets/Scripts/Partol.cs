0c677db8ec57243ea4f5ff4f94e0e80a4a135dfa
CasperJSUSUDX <casper.tw.ma@gmail.com>
2025-12-03 15:45:26 +0800

    Add more random on patrol and finish basic animator transition

--- FunRPG/Assets/Scripts/Partol.cs
+++ FunRPG/Assets/Scripts/Partol.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor.Animations;
+using UnityEngine.Rendering;
 
 public class Partol : MonoBehaviour
 {
@@ -12,26 +13,32 @@ public class Partol : MonoBehaviour
     private int timer = 1000;
     void Start()
     {
-        partolRange = new float[,] { {tf.position.x - 10f, tf.position.x + 10f}, {tf.position.y - 10f, tf.position.y + 10f} };
+        partolRange = new float[,] { {tf.position.x - 10f, tf.position.x + 10f}, {tf.position.z - 10f, tf.position.z + 10f} };
     }
 
     // Update is called once per frame
     void Update()
     {
-
         timer -= 1;
         if (timer < 0 ||
             (tf.position.x > partolRange[0, 1] && tf.position.x < partolRange[0, 0]) ||
-            (tf.position.y > partolRange[1, 1] && tf.position.y < partolRange[1, 0])
+            (tf.position.z > partolRange[1, 1] && tf.position.z < partolRange[1, 0])
         )
         {
             timer = 1000;
             float deg = Random.Range(0f, 6.28f);
-            rb.linearVelocity = new Vector3(Mathf.Cos(deg) * speed, 0, Mathf.Sin(deg) * speed);
-        }
-        if (Vector3.Magnitude(rb.linearVelocity) > 0)
-        {
-            anmiator.SetTrigger("TrRun");
+            float idle = Random.Range(0f, 1f);
+            if (idle > 0.5f)
+            {
+                rb.linearVelocity = new Vector3(Mathf.Cos(deg) * speed, 0, Mathf.Sin(deg) * speed);
+                anmiator.SetBool("haveSpeed", true);
+            }
+            else
+            {
+                rb.linearVelocity = new Vector3(0, 0, 0);
+                anmiator.SetBool("haveSpeed", false);
+            }
+            
         }
     }
 }
